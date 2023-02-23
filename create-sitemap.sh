989ef928dbe394e77d0dc64689f2097d1f7e2460
Kim, Hwan <akon47@naver.com>
2023-02-23 22:37:02 +0900

    Update .env

--- create-sitemap.sh
+++ create-sitemap.sh
@@ -4,4 +4,6 @@ RELATIVE_DIR=`dirname "$0"`
 cd $RELATIVE_DIR
 SHELL_PATH=`pwd -P`
 
+rm -rf /var/www/html/static/*
+
 node $SHELL_PATH/build/index.js -sitemap-out /var/www/html/sitemap/sitemap.xml -static-out /var/www/html/static
